fdda1bc8dcb6faff50539fe9138b503e657ca299
Binh Bui <binhbv@gmail.com>
2022-09-29 11:10:37 +0700

    release

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -4,10 +4,10 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 describe('must crawl fromUrl correct', function () {
-  it.skip('should set error when url is not found', async function () {
+  it('should set error when url is not found', async function () {
     let url = 'https://www.google.com/404';
     try {
-      await fromUrl(url);
+      await fromUrl({ url });
       // Fail test if above expression doesn't throw anything.
       expect(true).toBe(false);
     } catch (e: any) {
@@ -18,21 +18,21 @@ describe('must crawl fromUrl correct', function () {
   it('should get data when url is pdf', async function () {
     let url =
       'https://www.digital.go.jp/assets/contents/node/basic_page/field_ref_resources/d6cfdcdd-75e4-460c-9ec0-af4f952e03d5/20210906_meeting_promoting_01.pdf';
-    const res = await fromUrl(url);
-    console.log(res);
+    const res = await fromUrl({ url });
+    // console.log(res);
     expect(res).not.toBe('');
   });
 
-  it.skip('should get data when url is html', async function () {
+  it('should get data when url is html', async function () {
     let url =
       'https://www.city.fukuoka.lg.jp/hofuku/coronavaccine/omicron_wakutin.html';
-    const res = await fromUrl(url);
-    console.log(res);
+    const res = await fromUrl({ url });
+    // console.log(res);
     expect(res).not.toBe('');
   });
 });
 
-describe.skip('fromBufferWithMimeType must work correct', () => {
+describe('fromBufferWithMimeType must work correct', () => {
   it('test local file pdf', async () => {
     let file = fs.readFileSync(path.join(__dirname, 'sample.pdf'));
     const res = await fromBufferWithMimeType(file, 'application/pdf');
@@ -46,35 +46,34 @@ describe.skip('fromBufferWithMimeType must work correct', () => {
   });
 });
 
-describe.skip('fromBuffer must work correct', () => {
+describe('fromBuffer must work correct', () => {
   it('test local file pdf', async () => {
     let file = fs.readFileSync(path.join(__dirname, 'sample.pdf'));
     const res = await fromBuffer(file);
-    console.log(res);
+    // console.log(res);
     expect(res).not.toBeNull();
   });
 
   it('test local html file', async () => {
     let file = fs.readFileSync(path.join(__dirname, 'sample.html'));
     const res = await fromBuffer(file);
-    console.log(res);
+    // console.log(res);
     expect(res).not.toBeNull();
   });
 });
 
-
-describe.skip('test image', () => {
+describe('test image', () => {
   it('should detect text from local image', async () => {
     let file = fs.readFileSync(path.join(__dirname, 'sample.png'));
     const res = await fromBuffer(file);
-    console.log(res);
+    // console.log(res);
     expect(res).not.toBeNull();
   });
 
   it('should detect text from remote image', async () => {
     let url = 'https://tesseract.projectnaptha.com/img/eng_bw.png';
-    const res = await fromUrl(url);
-    console.log(res);
+    const res = await fromUrl({ url });
+    // console.log(res);
     expect(res).not.toBeNull();
   });
 });
